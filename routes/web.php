c112a8032f7bead8a0a1a09a8baf84e90a551e76
Zhenka-H <zhenka.hovhannisyan@gmail.com>
2020-06-13 17:59:41 +0400

    commited

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', "ContactController@index") -> name("contacts.index");
+Route::get('/', "ContactController@index") -> name("contacts.index") -> middleware('auth');
 
 Route::post('/', "ContactController@store") -> name("contacts.store");
 
-Route::get('/contacts/create', "ContactController@create") -> name("contacts.create");
+Route::get('/contacts/create', "ContactController@create") -> name("contacts.create") -> middleware('auth');
 
 Route::get('/contacts/{id}', "ContactController@show") -> name("contacts.show");
+
+Route::get('/contacts/{id}/edit', "ContactController@edit") -> name("contacts.edit");
 
 Route::put('/contacts/{id}', "ContactController@update") -> name("contacts.update");
 
 Route::delete('/contacts/{id}', "ContactController@destroy") -> name("contacts.destroy");
 
-Route::get('/contacts/{id}/edit', "ContactController@edit") -> name("contacts.edit");
 
 
 
@@ -30,3 +31,7 @@ Route::get('/profile', function(){
 
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
