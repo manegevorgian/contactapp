e8f88fed7350b7bc1489ee0b284f2baf4502cb4c
manegevorgian <manegevorgyan00@gmail.com>
2020-06-16 01:43:29 +0400

    message

--- app/User.php
+++ app/User.php
@@ -1,9 +1,13 @@
 <?php
 
 namespace App;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\CanResetPassword;
+
 
 class User extends Authenticatable
 {
@@ -22,4 +26,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+//    public function sendPasswordResetNotification($token)
+//    {
+//        $this->notify(new ResetPasswordController($token));
+//    }
 }
